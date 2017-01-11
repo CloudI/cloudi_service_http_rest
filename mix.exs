4e4477f451bcfe11e4a99e1b35221d65544c1b45
Michael Truog <mjtruog@gmail.com>
2017-01-10 20:17:22 -0800

    Version 1.6.0.

--- mix.exs
+++ mix.exs
@@ -3,16 +3,16 @@ defmodule CloudIServiceHttpRest do
 
   def project do
     [app: :cloudi_service_http_rest,
-     version: "1.5.4",
+     version: "1.6.0",
      language: :erlang,
-     description: description,
-     package: package,
-     deps: deps]
+     description: description(),
+     package: package(),
+     deps: deps()]
   end
 
   defp deps do
-    [{:cloudi_core, "~> 1.5.4"},
-     {:trie, "~> 1.5.4"}]
+    [{:cloudi_core, "~> 1.6.0"},
+     {:trie, "~> 1.6.0"}]
   end
 
   defp description do

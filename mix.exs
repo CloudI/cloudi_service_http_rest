29bef02f8b6ab8e605a7211c0ee993121777963f
Michael Truog <mjtruog@protonmail.com>
2019-11-28 15:22:37 -0800

    Version 1.8.0.

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule CloudIServiceHttpRest do
 
   def project do
     [app: :cloudi_service_http_rest,
-     version: "1.7.5",
+     version: "1.8.0",
      language: :erlang,
      erlc_options: [
        :debug_info,
@@ -26,8 +26,8 @@ defmodule CloudIServiceHttpRest do
   end
 
   defp deps do
-    [{:cloudi_core, "~> 1.7.5"},
-     {:trie, "~> 1.7.5"}]
+    [{:cloudi_core, "~> 1.8.0"},
+     {:trie, "~> 1.8.0"}]
   end
 
   defp description do
